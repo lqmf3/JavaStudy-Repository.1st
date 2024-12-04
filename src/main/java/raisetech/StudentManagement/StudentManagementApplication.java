98c3f3d6a81b36981cd747af53a366455f2d54f6
Akaike <oike1112@gmail.com>
2024-12-04 22:19:29 +0900

    スタンダード第10回演習課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,10 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -28,30 +18,12 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam("name")String name) {
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-
-	}
-	@PatchMapping("/student")
-	public void upStudentName(String name, int age){
-		repository.updateStudent(name, age);
-	}
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
-	@GetMapping("/students")
-	public String getAllStudents(){
-		List<Student> students = repository.findAll();
-		return students.stream()
-				.map(student -> student.getName() + "," + student.getAge() + "歳")
-				.collect(Collectors.joining("},{", "{", "}"));
+	@GetMapping("/studentsCoursesList")
+	public List<StudentsCourses> getStudentsCoursesList() {
+		return repository.findAllStudentsCourses();
 	}
 }
