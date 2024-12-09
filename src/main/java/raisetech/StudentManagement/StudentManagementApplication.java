bf5b470e97f5e440991fa7bf70723e8e0243fd41
Akaike <oike1112@gmail.com>
2024-12-09 23:36:27 +0900

    スタンダード第11回演習課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -6,24 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
-@RestController
 public class StudentManagementApplication {
 
-	@Autowired
-	private  StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-	@GetMapping("/studentsCoursesList")
-	public List<StudentsCourses> getStudentsCoursesList() {
-		return repository.findAllStudentsCourses();
 	}
 }
