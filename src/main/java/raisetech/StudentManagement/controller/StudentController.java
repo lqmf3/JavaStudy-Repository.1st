186e667fac45695d4a864d938f306e7b030de531
Akaike <oike1112@gmail.com>
2024-12-28 19:55:39 +0900

    スタンダード第15回演習課題

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,10 +3,15 @@ package raisetech.StudentManagement.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.naming.Binding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.Domain.StudentDetail;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
@@ -39,5 +44,26 @@ public class StudentController {
   private static List<StudentDetail> getStudentDetails(List<StudentDetail> studentDetails) {
     return studentDetails;
   }
-}
 
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+    //　新規受講生情報を登録する処理を実装する。
+    //　コース情報も一緒に実装できるように実装する。コースは単体で良い。
+
+    if (studentDetail.getStudentsCourses() == null || studentDetail.getStudentsCourses().isEmpty()) {
+      result.rejectValue("studentsCourses", "error.studentsCourses", "コース情報を入力してください。");
+      return "registerStudent";
+      }
+
+      return "redirect:/studentList";
+    }
+  }
