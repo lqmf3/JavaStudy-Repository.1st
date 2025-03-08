d4073bbff00e55643fc87f34958d2fca31939947
Akaike <oike1112@gmail.com>
2025-03-08 20:32:11 +0900

    スタンダード第29回演習課題-2 ・ワイルドカード * でのimportを修正(importの整理)

--- src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
@@ -1,13 +1,12 @@
 package raisetech.StudentManagement.controller.converter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.MockMvc;
 import raisetech.StudentManagement.Domain.StudentDetail;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
